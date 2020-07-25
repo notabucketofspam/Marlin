229ab5d84385151cf315555360efcc5a6e106b1f
notabucketofspam <not.a.bucket.of.spam@gmail.com>
2020-07-25 13:01:37 -0400

    Z2 stepper

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2255,7 +2255,7 @@
   #if AXIS_IS_TMC(Z2)
     #define Z2_CURRENT      800
     #define Z2_CURRENT_HOME Z2_CURRENT
-    #define Z2_MICROSTEPS    16
+    #define Z2_MICROSTEPS    128
     #define Z2_RSENSE         0.11
     #define Z2_CHAIN_POS     -1
   #endif
